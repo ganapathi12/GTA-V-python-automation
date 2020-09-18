5e74d9d608b806df99d02af71f394f27b6dbc0c9
Balaji D <balajidass07@gmail.com>
2020-09-18 20:51:10 +0530

    Update p3.py

--- p3.py
+++ p3.py
@@ -2,43 +2,17 @@ from PIL import ImageGrab
 import numpy as np
 import cv2
 import time
-# import pyautogui
 from directkeys import PressKey, W,A,S,D,ReleaseKey
-
-for i in list(range(4))[::-1]:
-	print(i+1)
-	time.sleep(1)
-
-
-print('down')
-PressKey(W)
-time.sleep(3)
-print('up')
-ReleaseKey(W)
-
-def process_img(original_image):
-	processed_img=cv2.cvtColor(original_image,cv2.COLOR_BGR2GRAY)
-	processed_img=cv2.Canny(processed_img,threshold1=200,threshold2=300)
-	return processed_img
-
-
-# for i in list(range(4))[::-1]:
-# 	print(i+1)
-# 	time.sleep(1)
-
-# last_time=time.time()
-# while(True):
-# 	screen=np.array(ImageGrab.grab(bbox=(0,40,800,640)))
-# 	new_screen=process_img(screen)	
-# 	print('down')
-# 	PressKey(W)
-# 	time.sleep(3)
-# 	print('up')
-# 	ReleaseKey(W)
-# 	print('Loop took {} seconds'.format(time.time()-last_time))
-# 	last_time=time.time()
-# 	cv2.imshow('window', new_screen)
-# 	# cv2.imshow('window',cv2.cvtColor(screen,cv2.COLOR_BGR2RGB))
-# 	if cv2.waitKey(25) & 0xFF==ord('q'):
-# 		cv2.destroyAllWindows()
-# 		break
+def main():
+	while(True):
+		img=np.array(ImageGrab.grab(bbox=(0,40,800,640)))
+        img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        img=cv2.Canny(img,threshold1=200,threshold2=300)
+        vertices=np.array([[10,500],[10,300],[300,200],[500,200],[800,300],[800,500]])
+        roi=np.zeros_like(img)
+        cv2.fillPoly(roi,vertices,255)
+        roi=cv2.bitwise_and(img,roi)
+		cv2.imshow('window', roi)
+		if cv2.waitKey(25) & 0xFF==ord('q'):
+			cv2.destroyAllWindows()
+			break
